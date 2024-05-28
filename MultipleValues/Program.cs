efdc8516e4c964e968ae3212116ef4ee092ed3f0
khyati <khyati.rani@cengage.com>
2024-05-28 21:53:44 +0530

    MutipleValuesReturn

---
+++ MultipleValues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class MultipleValues
+
+{
+    private static void fun(ref int x, ref int y)
+    {
+        x = 1;
+        y = 2;
+    }
+    public static void Main()
+    {
+        int x = 0;
+        int y = 0;
+        fun(ref x, ref y);
+        Console.WriteLine("x = {0}, y = {1}", x, y);
+    }
+}
